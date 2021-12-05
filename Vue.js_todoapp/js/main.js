c356051b90cb0bd621182097ee67fd15e528d456
Shota Okugawa <okusho721@gmail.com>
2021-12-05 12:40:50 +0900

    todo App step1 done.

--- Vue.js_todoapp/js/main.js
+++ Vue.js_todoapp/js/main.js
@@ -1,4 +1,19 @@
 const app = Vue.createApp({
-  data: () => ({}),
+  data: () => ({
+    newItem: "",
+    todos: [],
+  }),
+  methods: {
+    addItem: function (event) {
+      // console.log("clicked!");
+      if (this.newItem === "") return;
+      let todo = {
+        item: this.newItem,
+        isDone: false,
+      };
+      this.todos.push(todo);
+      this.newItem = "";
+    },
+  },
 });
 app.mount("#app");
