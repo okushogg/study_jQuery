671f3be4ab0dcd6923ee08ae90b68afeba571631
Shota Okugawa <okusho721@gmail.com>
2021-12-06 22:19:53 +0900

    v-on

--- Practice/js/practice.js
+++ Practice/js/practice.js
@@ -63,7 +63,6 @@
 // });
 // app.mount("#app");
 
-
 // section 20 v-model
 // const app = Vue.createApp({
 //   data: () => ({
@@ -72,14 +71,25 @@
 // })
 // app.mount('#app')
 
+// section 21
+// const app = Vue.createApp({
+//   data: () => ({
 
-// section 21 
+//   })
+// })
+// app.component('hello-component',{
+//   template: '<p>Hello!</p>'
+// })
+// app.mount('#app')
+
 const app = Vue.createApp({
   data: () => ({
-
-  })
-})
-app.component('hello-component',{
-  template: '<p>Hello!</p>'
-})
-app.mount('#app')
+    message: "Hello, Vue.js!",
+  }),
+  methods: {
+    clickHandler: function (event) {
+      this.message = this.message.split("").reverse().join("");
+    },
+  },
+});
+app.mount("#app");
